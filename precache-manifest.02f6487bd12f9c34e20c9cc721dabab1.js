6daaa697a3795f617d795742be47c107f2baa64b
Anusha534 <kothagundla.anu@gmail.com>
2020-06-23 22:57:32 +0530

    Updates

---
+++ precache-manifest.02f6487bd12f9c34e20c9cc721dabab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99424a255e7cb34d4786192456e7cfc5",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "6566458ef93bba0ae3ee",
+    "url": "/react-gh-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8a985db1741ab996abb6",
+    "url": "/react-gh-pages/static/js/2.84e09d9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.84e09d9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6566458ef93bba0ae3ee",
+    "url": "/react-gh-pages/static/js/main.ce844526.chunk.js"
+  },
+  {
+    "revision": "66e191b5fd3a61b4dd4a",
+    "url": "/react-gh-pages/static/js/runtime-main.732442c1.js"
+  }
+]);
